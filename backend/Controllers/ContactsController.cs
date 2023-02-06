3da880e264c77ebdf9d652b477e4043c5e8ef000
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-06 14:11:39 +0800

    Completed backend api

---
+++ backend/Controllers/ContactsController.cs
@@ -0,0 +1,74 @@
+﻿using API.DTOs;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        // Inject JsonContactsService
+        private JsonFileService _service;
+
+        public ContactsController(JsonFileService service)
+        {
+            _service = service;
+        }
+
+        // CRUD HTTP Requests Handler
+
+        // Create a read contact http get request handler
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(_service.GetContacts());
+        }
+
+        // Create a read contact http get request handler
+        [HttpGet("{id}")]
+        public IActionResult GetContactById(Guid id)
+        {
+            var contact = _service.GetContactById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+
+        // Create a create contact http post request handler
+        [HttpPost]
+        public IActionResult AddContact(CreateContactDTO createContact)
+        {
+            var contact = _service.AddContact(createContact);
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + contact.Id, contact);
+        }
+
+        // Create a update contact http put request handler
+        [HttpPut("{id}")]
+        public IActionResult UpdateContact(Guid id, UpdateContactDTO updateContact)
+        {
+            var contact = _service.GetContactById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            _service.UpdateContact(updateContact);
+            return NoContent();
+        }
+
+        // Create a delete contact http delete request handler
+        [HttpDelete("{id}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = _service.GetContactById(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            _service.DeleteContact(id);
+            return NoContent();
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,18 @@
+using API.Data;
+using API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// add the JsonFileService as a scoped
+builder.Services.AddScoped<JsonFileService>();
+
+// Add the JsonFileRepository as scoped
+builder.Services.AddScoped<JsonFileRepository>();
+
+// add automapper as scoped
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +27,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Frontend (ReactTS) => React runs on localhost:3000
+
+// add a cors policy to accept http requests from localhost:3000
+app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
 
 app.UseAuthorization();
 

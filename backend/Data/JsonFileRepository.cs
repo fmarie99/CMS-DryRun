93c70e58aefe42b7e443a7636a9a6a492f4d7eb3
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-06 13:38:51 +0800

    Created contacts.json DB and data access layer

---
+++ backend/Data/JsonFileRepository.cs
@@ -0,0 +1,67 @@
+﻿using API.Models;
+using System.Text.Json;
+
+namespace API.Data
+{
+    public class JsonFileRepository
+    {
+        public List<Contact> Contacts{ get; set; }
+
+        // create a repository that reads content inside a contacts.json and stores it on Contacts property
+        public JsonFileRepository()
+        {
+            var json = File.ReadAllText("contacts.json");
+            Contacts = JsonSerializer.Deserialize<List<Contact>>(json);
+        }
+
+        // CRUD opertations
+
+        // Create a method to get all contacts
+        public IEnumerable<Contact> GetAllContacts()
+        {
+            return Contacts;
+        }
+
+        // Create a method to get a contact by id
+        public Contact GetContactById(Guid id)
+        {
+            return Contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        // Create a method to add a new contact
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+            
+            SaveChanges();
+        }
+
+        // create a method to update a contact
+        public void UpdateContact(Contact contact)
+        {
+            var index = Contacts.FindIndex(c => c.Id == contact.Id);
+            Contacts[index] = contact;
+
+            SaveChanges();
+        }
+
+        // create a method to delete a contact
+        public void DeleteContact(Guid id)
+        {
+            var index = Contacts.FindIndex(c => c.Id == id);
+            Contacts.RemoveAt(index);
+
+            SaveChanges();
+        }
+
+        // create a method to save changes
+        public void SaveChanges()
+        {
+            var json = JsonSerializer.Serialize(Contacts, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+            File.WriteAllText("contacts.json", json);
+        }
+    }
+}

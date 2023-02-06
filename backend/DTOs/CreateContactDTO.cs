ebdb068e82ad8afe4572e51b5800b516eb614773
fmarie <fmarie@propio-ls.com>
2023-02-06 13:25:44 +0800

    Added model and DTOs

---
+++ backend/DTOs/CreateContactDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTOs
+{
+    public class CreateContactDTO
+    {
+        //Create a CreateContactDTO that has the properties Firstname, lastname, physical address and delivery address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+    }
+}
---
+++ backend/DTOs/ReadContactDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DTOs
+{
+    public class ReadContactDTO
+    {
+        //Create a ReadContactDTO that has the properties id of type GUID, Firstname, lastname, physical address and delivery address
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string DeliveryAddress { get; set; }
+    }
+}

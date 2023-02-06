068be94cccbf3fb8ea34a70125586d38c3922def
Vincent Nadal <vnadal@fullscale.io>
2023-02-06 14:51:36 +0800

    Add api calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,27 @@ import React from "react";
 import ContactList from "./components/ContactList";
 import { useState } from "react";
 import ContactForm from "./components/ContactForm";
+import { Contact } from "./interfaces/Contact";
 
 function App() {
   // create a state variable for contacts
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  const setContactsHandler = (contacts: Contact[]) => {
+    setContacts(contacts);
+  };
+
+  const addContactHandler = (contact: Contact) => {
+    setContacts((prevContacts) => {
+      return [...prevContacts, contact];
+    });
+  };
 
   return (
     <div className="App">
-      <h1>Contact Management App</h1>
-      <ContactList contacts={contacts} />
-      <ContactForm />
+      <h1>Contact Management Application</h1>
+      <ContactForm handleAddContact={addContactHandler}/>
+      <ContactList contacts={contacts} handleSetContacts={setContactsHandler} />
 
       <p>© chan, tim, mac</p>
     </div>
--- frontend/src/components/ContactList.tsx
+++ frontend/src/components/ContactList.tsx
@@ -1,17 +1,30 @@
+import React, { useEffect } from "react";
 import { Contact } from "../interfaces/Contact";
+import { getContacts } from "../services/Api";
 
 interface Props {
   contacts: Contact[];
+  handleSetContacts: (contacts: Contact[]) => void;
 }
 
-export default function ContactList({ contacts }: Props) {
+export default function ContactList({ contacts, handleSetContacts }: Props) {
+  // Read the contacts from the API using the useEffect hook with GetAllContacts
+
+  useEffect(() => {
+    // fetch the contacts from the API
+    // set the contacts in the state
+    (async () => {
+      handleSetContacts(await getContacts());
+    })();
+  }, []);
+
   // create a component that displays list of contacts using UL and LI tags
   // add a button to each contact to delete it
   return (
     <div>
       <ul>
         {contacts.map((contact) => (
-          <li>
+          <li key={contact.id}>
             {contact.firstName} {contact.lastName}
             <div>
               {contact.physicalAddress}
